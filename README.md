f00a31547634e9aaeab2a007f2e48b62c964da57
virendra.sehrawat <virendra.sehrawat@tavant.com>
2020-09-05 22:52:42 +0530

    make it better

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Heroku Flask is a simple web app programmed in Python-3 using flask micro-framework. 
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__, static_folder='templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['GET'])
+def getCategory():
+    try:
+        #data = request.json
+        return  str(request.args['query']) #//str(data)
+    except :
+        return str(request.args['query'])
+    
+    
+
+
+
+
+if __name__ == '__main__': app.run(debug=True)
